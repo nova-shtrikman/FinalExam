e46207e37a3b123a1fc1e6300d55beec7b4eff47
Nova <nova.shtrikman@wsu.edu>
2023-04-26 19:05:23 +0000

    trying to figure out how to load recipes into menu itmes within drop down menu

--- scripts.js
+++ scripts.js
@@ -46,3 +46,81 @@ function showSlides(n) {
 }
 
 
+
+
+
+
+
+function showRecipe(recipeName) {
+    // Load the recipe information based on the recipe name
+    switch(recipeName) {
+      case "GF Chocolate Cake":
+        // Load information for GF Chocolate Cake
+        console.log("cake-directions.html", "cake-ingredients.html");
+        break;
+      case "Magic Bars":
+        // Load information for Magic Bars
+        console.log("Information for Magic Bars loaded!");
+        break;
+      case "Chocolate Chip Cookies":
+        // Load information for Chocolate Chip Cookies
+        console.log("Information for Chocolate Chip Cookies loaded!");
+        break;
+      default:
+        console.log("Invalid recipe name.");
+        break;
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//define a recipe object constructor 
+
+//function Recipe(a, b, c) {
+
+    //set some object properties
+    //this.name = a;
+    //this.ingredientsFile = b; //file name to the HTML snippet containing this recipe's ingredient list
+    //this.directionsFile = c; //file name to the HTML snippet containing this recipe's directions list
+
+//update the display with the content for this recipe
+//this.display = function() {
+    //document.querySelector("#hero h1").innerHTML= this.name;
+    //loadFileInto(this.ingredientsFile , '#ingredients');
+   // loadFileInto(this.directionsFile, '#directions');
+
+} //end of .display() method
+
+//adds this recipe to the Nav Bar ul as a new li tag that is clickable
+//this.addTonmyDropdown = function() {
+        
+    //create a new element for the navbar
+    //let newNavLI = document.createElement("li");
+    //newNavLI.innerHTML = this.name;
+    //document.querySelector("#navbar ul").appendChild(newNavLI);
+
+    //preserve recipe self "this" in a different variable to use within the event listener
+    //let recipeSelf = this;
+    //newNavLI.addEventListener("click", function() {
+        //recipeSelf.display();
+    //});
+
+}//end of .addToNav method
+
+} //end Recipe () object constructor
+
+
+
+
