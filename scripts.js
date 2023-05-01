cba918a6bb3c9fd575f5cc10b5cb5316508fc396
Nova <nova.shtrikman@wsu.edu>
2023-05-01 18:08:07 +0000

    trying to figure out how to link files

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+/*JS for the nag bar*/
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 function myFunction() {
@@ -13,12 +14,14 @@ function myFunction() {
       }
     }
   }
+  /*end JS for nav bar*/
 
 
-  let slideIndex = 1;
+/* JS for slideshow */
+
+let slideIndex = 1;
 showSlides(slideIndex);
 
-// changes picture when next button is clicked
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
@@ -30,97 +33,40 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("demo");
-  let captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i].style.display = "none";  
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
 }
 
+/*End JS for slideshow*/
 
 
 
+//document.getElementById("dropdown").addEventListener("click", function() {
+   // window.location.href = "cake-ingredients" , "cake-directions";
+  //});
 
-
-
-function showRecipe(recipeName) {
-    // Load the recipe information based on the recipe name
-    switch(recipeName) {
-      case "GF Chocolate Cake":
-        // Load information for GF Chocolate Cake
-        console.log("cake-directions.html", "cake-ingredients.html");
-        break;
-      case "Magic Bars":
-        // Load information for Magic Bars
-        console.log("Information for Magic Bars loaded!");
-        break;
-      case "Chocolate Chip Cookies":
-        // Load information for Chocolate Chip Cookies
-        console.log("Information for Chocolate Chip Cookies loaded!");
-        break;
-      default:
-        console.log("Invalid recipe name.");
-        break;
-    }
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//define a recipe object constructor 
-
-//function Recipe(a, b, c) {
-
-    //set some object properties
-    //this.name = a;
-    //this.ingredientsFile = b; //file name to the HTML snippet containing this recipe's ingredient list
-    //this.directionsFile = c; //file name to the HTML snippet containing this recipe's directions list
-
-//update the display with the content for this recipe
-//this.display = function() {
-    //document.querySelector("#hero h1").innerHTML= this.name;
-    //loadFileInto(this.ingredientsFile , '#ingredients');
-   // loadFileInto(this.directionsFile, '#directions');
-
-} //end of .display() method
-
-//adds this recipe to the Nav Bar ul as a new li tag that is clickable
-//this.addTonmyDropdown = function() {
+  
+ //adds this recipe to the Nav Bar ul as a new li tag that is clickable
+ this.addToNav = function() {
         
     //create a new element for the navbar
-    //let newNavLI = document.createElement("li");
-    //newNavLI.innerHTML = this.name;
-    //document.querySelector("#navbar ul").appendChild(newNavLI);
+    let newNavLI = document.createElement("a");
+    newNavLI.innerHTML = this.name;
+    document.querySelector("#myDrowndown").appendChild(newNavLI);
 
     //preserve recipe self "this" in a different variable to use within the event listener
-    //let recipeSelf = this;
-    //newNavLI.addEventListener("click", function() {
-        //recipeSelf.display();
-    //});
+    let recipeSelf = this;
+    newNavLI.addEventListener("click", function() {
+        recipeSelf.display();
+    });
 
-}//end of .addToNav method
-
-} //end Recipe () object constructor
-
-
-
-
+}//end of .addToNav method 
